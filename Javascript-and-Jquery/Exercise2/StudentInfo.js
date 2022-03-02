3f2a9e9b5a5927a63269fc3521e7050a9db3a6b9
Pooja Yadav <100533707+YadavPooja-29@users.noreply.github.com>
2022-03-02 19:43:55 +0530

    Create StudentInfo.js

---
+++ Javascript-and-Jquery/Exercise2/StudentInfo.js
@@ -0,0 +1,20 @@
+function validate()
+{
+	var sname = document.getElementById("name").value;
+	var sage = document.getElementById("age").value;
+	var syop = document.getElementById("yop").value;
+	var scollege = document.getElementById("college").value;
+	
+	if ( sname == "Pooja" && sage == 21 && syop == 2022 && scollege == "Jawaharlal Nehru Engineering College")
+	{
+		alert ("Welcome Dear Student..!!!\n"+ "Name :"+sname+ "\nAge :"+sage+ "\nYear of Passing :"+syop+ "\nCollege Name : "+scollege);
+		window.location = "StudentInfo.html"; // Redirecting to other page.
+		return false;
+	}
+	else
+	{
+		alert ("\nPlease provide Valid Credential..!!!);
+		window.location = "StudentInfo.html"; // Redirecting to other page.
+		return false;
+	}
+}
