43e21b67db4a40a6bff5d49911886dc70fe32123
Pooja Yadav <100533707+YadavPooja-29@users.noreply.github.com>
2022-03-03 11:56:09 +0530

    Update StudentInfo.js

--- Javascript-and-Jquery/Exercise2/StudentInfo.js
+++ Javascript-and-Jquery/Exercise2/StudentInfo.js
@@ -1,14 +1,9 @@
 function validate()
 {
-	var sname = document.getElementById("name").value;
-	var sage = document.getElementById("age").value;
-	var syop = document.getElementById("yop").value;
-	var scollege = document.getElementById("college").value;
+	var sname = document.getElementById("name");
+	var sage = document.getElementById("age");
+	var syop = document.getElementById("yop");
+	var scollege = document.getElementById("college");
 	
-	if ( sname == "Pooja" && sage == 21 && syop == 2022 && scollege == "Jawaharlal Nehru Engineering College")
-	{
-		alert ("Welcome Dear Student..!!!\n"+ "Name :"+sname+ "\nAge :"+sage+ "\nYear of Passing :"+syop+ "\nCollege Name : "+scollege);
-		window.location = "StudentInfo.html"; // Redirecting to same page.
-		return false;
-	}
+	alert ("Welcome Dear Student..!!!\n"+ "Name : "+sname.value+ "\nAge : "+sage.value+ "\nYear of Passing : "+syop.value+ "\nCollege Name : "+scollege.value);
 }
